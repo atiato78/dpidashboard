36cb557887b7b0083073892012b32cb89d1c5b70
atiato78 <40871759+atiato78@users.noreply.github.com>
2019-02-22 20:04:07 +0200

    Update ahm_mobilock_imei_function.js

--- dpidashboard/ahm_mobilock_imei_function.js
+++ dpidashboard/ahm_mobilock_imei_function.js
@@ -7,7 +7,7 @@ $(function(){
 	function LoadData(){
 		var all_data = [];		
 		//$.getJSON("http://192.168.0.77:9999/kubernetes/dpidashboard/imei_api.php?function="+functionName+"&jsonCallback=?", function(data){
-		$.getJSON("http://localhost:8080/kubernetes/dpidashboard/mobilock_api.php?function="+functionName+"&jsonCallback=?", function(data){
+		$.getJSON("http://dpidashboard-expo.7e14.starter-us-west-2.openshiftapps.com/dpidashboard/mobilock_api.php?function="+functionName+"&jsonCallback=?", function(data){
 			
 			if(data==="error"){
 				alert("error");
